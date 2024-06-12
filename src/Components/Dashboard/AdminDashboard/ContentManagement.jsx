602f886b40266515c965f16a54ecb94b13db6c07
Salauddin Ahmed <110757660+Salauddin2k3@users.noreply.github.com>
2024-06-12 18:26:40 +0600

    draft filtering added

--- src/Components/Dashboard/AdminDashboard/ContentManagement.jsx
+++ src/Components/Dashboard/AdminDashboard/ContentManagement.jsx
@@ -2,23 +2,49 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import BLogPost from "./BLogPost";
+import { useState } from "react";
 
 
 const ContentManagement = () => {
 
-    const { data: posts = [] } = useQuery({
-        queryKey: ['posts'],
+    const [filter, setFilter] = useState('All');
+
+    const { data: posts = [], isLoading, error } = useQuery({
+        queryKey: ['posts', filter],
         queryFn: async () => {
             const res = await axios.get(`http://localhost:5000/allBLogPost`);
-            return res.data;
+            if (filter === 'All') return res.data;
+            return res.data.filter(post => post.status === filter.toLowerCase());
         }
     });
 
+    const handleFilterChange = (event) => {
+        setFilter(event.target.value);
+    };
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error loading posts</div>;
+    }
+
     return (
         <div>
-            <div className="flex justify-center">
+            <div className="flex justify-center items-center mt-6 gap-6">
+                <div className="">
+                    <div className="">
+                        <select value={filter} onChange={handleFilterChange} className="select select-bordered">
+                            <option disabled value="Select One">Select Option</option>
+                            <option value="All">All</option>
+                            <option value="Draft">Draft</option>
+                            <option value="Published">Published</option>
+                        </select>
+                    </div>
+                </div>
                 <div>
-                    <Link to="/dashboard/content-management/add-blog"><button className="btn bg-[#00929E] text-white mt-6 px-8 w-fit">Add Blog</button></Link>
+                    <Link to="/dashboard/content-management/add-blog"><button className="btn bg-[#00929E] text-white  px-8 w-fit">Add Blog</button></Link>
                 </div>
             </div>
             <div className="mt-20 flex justify-center">
